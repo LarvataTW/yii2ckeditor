16322693bb167d9133bc39d351021400784033c3
Meng-Yang Lee <crazylion2@gmail.com>
2015-05-30 23:18:43 +0800

    initial commit

---
+++ CkeditorAsset.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class CkeditorAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor';
+    public $css = [
+    ];
+    public $js = [
+        'ckeditor/ckeditor/ckeditor.js'
+    ];
+    public $depends = [
+    ];
+}
